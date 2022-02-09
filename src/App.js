ce342e03a4a812b76b1028687b9c27df34fe17b8
irumvanselme <andesanselme@gmail.com>
2022-02-09 10:02:33 +0200

    initialize cypress

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [isSubmitted, setIsSubmitted] = useState(false);
+
+    const handleFormSubmission = (event) => {
+        event.preventDefault();
+        setIsSubmitted(true);
+    };
+
+    return (
+        <div className="App">
+            <h1>fill this form to see the change</h1>
+            {isSubmitted && (
+                <div style={{ marginTop: 30, marginBottom: 30, color: "red" }}>
+                    Form is submitted
+                </div>
+            )}
+            <form onSubmit={handleFormSubmission}>
+                <input type="text" placeholder="Enter your names" /> <br />
+                <button type="submit">Submit the form</button>
+            </form>
+        </div>
+    );
 }
 
 export default App;
